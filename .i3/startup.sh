8c91ad61d20e9ddd7da7a70876c3a951e6fa9d5c
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2018-01-16 15:33:54 -0500

    .i3 updates
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@gmail.com>

--- .i3/startup.sh
+++ .i3/startup.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 # scratchpad
-urxvt -name urxvt-scratchpad &
-
-i3-msg "workspace 1t; append_layout ~/.i3/saved/1t.json"
+urxvtc -name urxvt-scratchpad &
 
 # 1t
-urxvt -name 1t-dmesg -e dmesg -w &
-urxvt -name 1t-free -e free -mh -s 10 &
-urxvt -name 1t-top -e top &
-urxvt -name 1t-0 -cd ~ &
-urxvt -name 1t-1 -cd ~ &
-urxvt -name 1t-2 -cd ~ &
+i3-msg "workspace 1t; append_layout ~/.i3/saved/1t.json"
+urxvtc -name 1t-dmesg -e dmesg -w &
+urxvtc -name 1t-free -e free -mh -s 10 &
+urxvtc -name 1t-top -e htop &
+urxvtc -name 1t-0 -cd ~ &
+urxvtc -name 1t-1 -cd ~ &
+urxvtc -name 1t-2 -cd ~ &
 
-i3-msg "workspace 2n; append_layout ~/.i3/saved/2n.json"
+
 # 2n
-google-chrome-stable &
+i3-msg "workspace 2n; append_layout ~/.i3/saved/2n.json"
+chromium-browser &
 
 # 3e
+i3-msg "workspace 3e; append_layout ~/.i3/saved/3e.json"
 emacs &
 
-xrandr_dwim
+# 5m
+i3-msg "workspace 5m; append_layout ~/.i3/saved/5m.json"
+slack &
+sametime-connect &
+
+# 8
+i3-msg "workspace 8; append_layout ~/.i3/saved/8.json"
+urxvtc -name weechat -e weechat &
