d76f32de2da3b7ebbde8f79457d2e219eed0cb2c
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2024-11-24 20:12:35 -0800

    Simplify scala-mode setup/config
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -353,9 +353,6 @@ Does nothing if `visual-line-mode' is on."
 
 ;;------------------ scala-mode
 
-;; Enable defer and ensure by default for use-package
-;; Keep auto-save/backup files separate from source code:  https://github.com/scalameta/metals/issues/1027
-
 ;; Enable scala-mode for highlighting, indentation and motion commands
 (use-package scala-mode
   :interpreter ("scala" . scala-mode))
@@ -396,24 +393,7 @@ Does nothing if `visual-line-mode' is on."
   (setq lsp-keep-workspace-alive nil))
 
 ;; Add metals backend for lsp-mode
-(use-package lsp-metals
-  :ensure t
-  :custom
-  ;; You might set metals server options via -J arguments. This might not always work, for instance when
-  ;; metals is installed using nix. In this case you can use JAVA_TOOL_OPTIONS environment variable.
-  (lsp-metals-server-args '(;; Metals claims to support range formatting by default but it supports range
-                            ;; formatting of multiline strings only. You might want to disable it so that
-                            ;; emacs can use indentation provided by scala-mode.
-                            "-J-Dmetals.allow-multiline-string-formatting=off"
-                            ;; Enable unicode icons. But be warned that emacs might not render unicode
-                            ;; correctly in all cases.
-                            "-J-Dmetals.icons=unicode"))
-  ;; In case you want semantic highlighting. This also has to be enabled in lsp-mode using
-  ;; `lsp-semantic-tokens-enable' variable. Also you might want to disable highlighting of modifiers
-  ;; setting `lsp-semantic-tokens-apply-modifiers' to `nil' because metals sends `abstract' modifier
-  ;; which is mapped to `keyword' face.
-  (lsp-metals-enable-semantic-highlighting t)
-  :hook (scala-mode . lsp))
+(use-package lsp-metals)
 
 ;; Enable nice rendering of documentation on hover
 ;;   Warning: on some systems this package can reduce your emacs responsiveness significally.
@@ -432,10 +412,10 @@ Does nothing if `visual-line-mode' is on."
 ;; To Company-lsp users:
 ;;   Company-lsp is no longer maintained and has been removed from MELPA.
 ;;   Please migrate to company-capf.
-;; (use-package company
-;;   :hook (scala-mode . company-mode)
-;;   :config
-;;   (setq lsp-completion-provider :capf))
+(use-package company
+  :hook (scala-mode . company-mode)
+  :config
+  (setq lsp-completion-provider :capf))
 
 ;; Posframe is a pop-up tool that must be manually installed for dap-mode
 (use-package posframe)
@@ -445,9 +425,6 @@ Does nothing if `visual-line-mode' is on."
   :hook
   (lsp-mode . dap-mode)
   (lsp-mode . dap-ui-mode))
-
-;; (setenv "PATH" (concat (getenv "PATH") ":~/Library/Application Support/Coursier/bin"))
-;; (setq exec-path (append exec-path '("~/Library/Application Support/Coursier/bin")))
 
 (setq scala-indent:align-forms t
       scala-indent:align-parameters t
